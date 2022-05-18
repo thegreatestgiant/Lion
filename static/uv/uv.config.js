1a0c9bcffd0d28392c4a7ece6ea79c4d189c893c
The Greatest Giant <96693544+thegreatestgiant@users.noreply.github.com>
2022-05-18 15:25:08 -0400

    Add files via upload

---
+++ static/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/fuck_you_gog/',
+    bare: '/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    fuck_you_gog: '/uv/uv.sw.js',
+};
