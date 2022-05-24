778ec4804f9d0466a010014090b587dadf31d983
The Greatest Giant <96693544+thegreatestgiant@users.noreply.github.com>
2022-05-24 07:42:19 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/fuck_you_gog/',
+    prefix: '/Foster/',
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
-    fuck_you_gog: '/uv/uv.sw.js',
+    Foster: '/uv/uv.sw.js',
 };
